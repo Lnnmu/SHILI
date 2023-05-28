89d8e33069488d74f0736c9e30355c1e2cf930be
Lnnmu <121074335+Lnnmu@users.noreply.github.com>
2023-05-28 13:06:47 +0800

    Add files via upload

--- chatBox.js
+++ chatBox.js
@@ -1,8 +1,9 @@
 const xhr = new XMLHttpRequest();
 const apiURL = 'https://api.pearktrue.cn/api/gpt/continuous.php?';
 const chatBox = document.querySelector('.chat-box');
-
+const main_box = document.getElementById('main');
 let golbalId;
+
 // 获取id
 function getId(callback) {
   const url = apiURL + 'type=get';
@@ -11,19 +12,27 @@ function getId(callback) {
     if (xhr.status === 200) {
       const response = JSON.parse(xhr.responseText);
       if (response.code === 200) {
+        // 这里使用了赋值给全局变量，又使用回调函数拿走id
         golbalId = response.id;
         callback(response.id);
-      } else {
-        setTimeout(getId, 1000); // 等待 1 秒后重新发送请求
       }
     }
   };
   xhr.send();
+  // 判断是否成功get到Id，如果没有就提示用户刷新网页
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 500) {
+      main_box.style.backgroundColor='red';
+      addMessage('error','请求过于频繁，请刷新网页重试');
+    }
+  };
 }
 
+// 主要是执行getId函数
 getId(function (id) {
   console.log(id);
   console.log(golbalId);
+  main_box.style.backgroundColor='green';
 });
 
 // 获取回复的内容并添加进会话
@@ -34,8 +43,8 @@ function getcontent(message, id) {
   xhr.onload = function () {
     if (xhr.status === 200) {
       const response = JSON.parse(xhr.responseText);
-      const answer = response.answer;
-      addMessage('AI', answer);
+      // console.log(response.answer);
+      addMessage('AI', response.answer);
       chatBox.scrollTop = chatBox.scrollHeight;
     }
   };
@@ -46,6 +55,11 @@ function getcontent(message, id) {
 function addMessage(user, text) {
   const message = document.createElement('div');
   message.classList.add('message');
+  text = text.replace(/</g, "&lt;");
+  text = text.replace(/>/g, "&gt;");
+  text = text.replace(/\n/g, "<br>");
+  text = text.replace(/```([\s\S]*?)```/g, "<pre><code>$1</code></pre>");
+  // console.log(text);
   message.innerHTML = `
     <p class="user">${user}:</p>
     <p class="text">${text}</p>
@@ -57,15 +71,19 @@ function addMessage(user, text) {
 function sendMessage() {
   var message = document.getElementById("input-type").value;
   var chatBox = document.querySelector('.chat-box');
+  if(message.trim() === ""){
+    return;
+  }
   addMessage('User', message);
   document.getElementById("input-type").value = "";
   getcontent(message, golbalId);
   chatBox.scrollTop = chatBox.scrollHeight;
 }
 
-function Enterkey(event){
+// 捕捉用户的回车，如果在输入框中回车则执行操作
+function Enterkey(event) {
   event = event || window.event;
-  if(event.keyCode == 13){
+  if (event.keyCode == 13) {
     sendMessage()
   }
 }
